651347c5041c441c0591facde00080523023c9ed
Mr741 <andregeada88@gmail.com>
2024-12-10 17:11:18 +0000

    Criação da ligação com a base de dados no site

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+    $host = "sql306.infinityfree.com";
+    $username = "if0_37806044";
+    $password = "8T75u2nNx5";
+    $database = "if0_37806044_XXX";
+?>
+
+<!DOCTYPE HTML>
+<html>
+    
+    <head>
+
+    </head>
+    
+    <body>
+        <main>
+            <?php 
+                foreach($filme on $flimes){
+
+                }
+            ?> 
+        </main>
+    </body>
+</html>
