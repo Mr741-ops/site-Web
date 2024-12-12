8aef7376219587a7c0cd9c87a19a8678e57d2293
Leonardo Silva <leofariatech@gmail.com>
2024-12-12 19:22:52 +0000

    Fix CSS

--- main.php
+++ main.php
@@ -29,7 +29,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmes</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
 
